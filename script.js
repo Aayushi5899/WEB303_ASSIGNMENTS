73581d2244d38575cb9b374d430e0d7f738014d2
Aayushi5899 <91238765+Aayushi5899@users.noreply.github.com>
2022-05-27 21:30:44 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 /*
 	WEB 303 Assignment 1 - jQuery
-	{Aayushi}
+	Aayushi Patel
 */
 
+$(document).ready(function(){
+	$("#price").keyup(function(){
+	  calculateTax();
+	});
+	$("#add-price").click(function(){
+		addToList();
+	  });
+	
+  });
+  
+  
+  function calculateTax(){
+	  let price = $("#price").val();
+	  let tax = price * 0.13 ;
+	  let printTax = tax.toFixed(2); 
+	  $("#tax").text('$'+printTax);
+  }
+
+  function addToList(){
+	let price = $("#price").val();
+	let tax = price * 0.13 ;
+	let total = Number(price) + Number(tax);
+	$("#price-list").append('<li>$'+total+'</li>');
+	console.log(total);
+  }
